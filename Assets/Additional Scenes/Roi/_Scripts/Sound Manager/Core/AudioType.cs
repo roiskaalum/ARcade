8c325aebc28dd501ed0d1756e2dee021a3fa27fc
Rói Skaalum <roi.skaalum@gmail.com>
2025-05-20 15:25:25 +0200

    changes to audio and roi folder structure

--- Assets/Additional Scenes/Roi/_Scripts/Sound Manager/Core/AudioType.cs
+++ Assets/Additional Scenes/Roi/_Scripts/Sound Manager/Core/AudioType.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 public enum AudioType
 {
     ST_01,
-    ST_02,
-    SFX_01,
-    SFX_02,
-    SFX_Footsteps,
+    Menu_SFX_01,
+    Can_SFX_01,
     None,
     //.. Custom audio types here
 }
